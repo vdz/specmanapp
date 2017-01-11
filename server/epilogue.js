90b960c0a7899fd4a828ca0287dda10107bd08ff
Yehuda Gilead <gilead.yehuda@gmail.com>
2017-01-11 23:45:26 +0200

    in init for epilogue

--- server/epilogue.js
+++ server/epilogue.js
@@ -1,10 +1,21 @@
 const db = require('./models');
 const bodyParser = require('body-parser');
 const epilogue = require('epilogue');
+const cors = require('cors');
 
 module.exports = {
     init: function(app) {
+        const corsOptions = {
+            optionsSuccessStatus : 200
+        };
 
+        app.use('/api', cors(corsOptions), function(req, res, next) {
+            if ('OPTIONS' == req.method) {
+                console.log(req.method);
+            }
+            next();
+        });
+        app.options('/api', cors(corsOptions));
         /*app.use(function(req, res, next) {
             res.header('Access-Control-Allow-Origin', '*');
             res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
