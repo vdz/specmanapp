82e3f8d949b4cf0e2ea2e5103ed32cc3b4773734
Yehuda Gilead <gilead.yehuda@gmail.com>
2017-03-13 12:18:25 +0200

    Payload too large fixed

--- server/epilogue.js
+++ server/epilogue.js
@@ -6,7 +6,8 @@ module.exports = {
     init: function(app) {
 
         app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json({limit: '50mb'}));
+        app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
         epilogue.initialize({
             app : app,
